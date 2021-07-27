cff6bfc385d00c3cb575da3c67553d4ee278e3ca
Julio José Marqués <87929993+JMarques98@users.noreply.github.com>
2021-07-27 12:13:45 +0200

    Update PDFEYES.py

--- PDFEYES.py
+++ PDFEYES.py
@@ -5,28 +5,28 @@
 
 
 import os
-#Modulo os para acceder a funcionalidades del Sistema Operativo,acceder a directorios,leer y escribir arhivos.
+#Module os to access Operating System functionalities, access directories, read and write files.
 import os.path
-#Submodulo de os, para acceder a funcionalidades relacionadas con los nombres de las rutas de archivos y directorios.
+#os submodule, to access functionalities related to file and directory path names.
 from PyPDF2 import PdfFileReader, PdfFileWriter
-# PyPDF2 es una libreria para trabajar con documentos PDF
-#PdfFileReader clase de PyPDF2 para obtener contenido del pdf
+# PyPDF2 is a library for working with PDF documents.
+#PdfFileReader class of PyPDF2 to get pdf content
 
 os.getcwd()
-#Devuelve una cadena que representa el directorio de trabajo actual 
+#Returns a string representing the current working directory 
 
 losarchivos = []
-#Se crea y se declara la lista donde vamos a meter los archivos
+#The list where we are going to put the files is created and declared.
 
 for file in os.listdir("."):
-    #Se listan todas las carpetas y archivos que tenemos en el escritorio y recorremos una por una
+    #All folders and files on the desktop are listed and we go through them one by one.
     if file.lower().endswith('.pdf'):
-        #Le decimos que si encuentra un archivo que termine con la extensión .pdf, añada el nombre a la lista de "losarchivos"
+        #We tell you that if you find a file ending with the extension .pdf, add the name to the list of "losarchivos".
         losarchivos.append(file)
 
 for i in range(len(losarchivos)):
     
-    #Recorremos la longitud de la lista y vamos extrayendo el texto de cada archivo
+    #We run through the length of the list and extract the text from each file.
     
     valor = str(losarchivos[i])
     pdf = PdfFileReader(valor)
@@ -36,7 +36,7 @@ for i in range(len(losarchivos)):
         
     print('El número de páginas de' + ' ' + valor + ' ' + 'es' + ' ' + str(number_of_pages))
     
-    #imprimimos por pantalla el número de paginas que se detecta en cada archivo
+    #we print by screen the number of pages detected in each file
     
     i = 0
     
@@ -45,10 +45,5 @@ for i in range(len(losarchivos)):
         print(page.extractText())
         i = i + 1
        
-
-
-# In[ ]:
-
-
 
 
